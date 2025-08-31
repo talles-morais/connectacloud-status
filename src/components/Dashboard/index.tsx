73a12ce9f7bffc9a533572b1b0762a31c8488b2e
Talles <talves2503@gmail.com>
2025-08-31 17:23:23 -0300

    feat: search filter

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -2,9 +2,11 @@ import { Search } from "lucide-react";
 import ServiceCard from "../ServiceCard";
 import useServiceStatus from "../../hooks/useServiceStatus";
 import ServiceCardSkeleton from "../ServiceCard/ServiceCardSkeleton";
+import { useState } from "react";
 
 export default function Dashboard() {
-  const { services, isLoading } = useServiceStatus()
+  const { services, isLoading } = useServiceStatus();
+  const [searchTerm, setSearchTerm] = useState("");
 
   return (
     <div className="container mx-auto pt-10 space-y-10">
@@ -14,22 +16,35 @@ export default function Dashboard() {
           type="text"
           placeholder="Pesquise pelo serviço desejado"
           className="outline-none flex-1"
+          onChange={e => setSearchTerm(e.target.value)}
         />
       </div>
 
       {/* service cards */}
       <section className="grid sm:grid-cols-2 md:grid-cols-3 gap-6 justify-center items-center">
-        {isLoading ? (
-          // Exibe skeletons enquanto carrega
-          Array.from({ length: 6 }).map((_, index) => (
-            <ServiceCardSkeleton key={index} />
-          ))
-        ) : (
-          // Exibe os serviços quando carregados
-          services.map(service => (
-            <ServiceCard key={service.id} service={service} />
-          ))
-        )}
+        {isLoading
+          ? // Exibe skeletons enquanto carrega
+            Array.from({ length: 6 }).map((_, index) => (
+              <ServiceCardSkeleton key={index} />
+            ))
+          : // Exibe os serviços quando carregados
+            (() => {
+              const filteredServices = services.filter(service =>
+                service.name.toLowerCase().includes(searchTerm.toLowerCase())
+              );
+
+              return filteredServices.length > 0 ? (
+                filteredServices.map(service => (
+                  <ServiceCard key={service.id} service={service} />
+                ))
+              ) : (
+                <div className="col-span-full text-center py-8">
+                  <span className="text-gray-500">
+                    Nenhum serviço encontrado
+                  </span>
+                </div>
+              );
+            })()}
       </section>
     </div>
   );
