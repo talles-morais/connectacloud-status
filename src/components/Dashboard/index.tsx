630a9308b96b9310d865cd5fdc3fc0e48d984d44
Talles <talves2503@gmail.com>
2025-08-31 17:06:32 -0300

    feat: skeleton and shadcn

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,6 +1,7 @@
 import { Search } from "lucide-react";
 import ServiceCard from "../ServiceCard";
 import useServiceStatus from "../../hooks/useServiceStatus";
+import ServiceCardSkeleton from "../ServiceCard/ServiceCardSkeleton";
 
 export default function Dashboard() {
   const { services, isLoading } = useServiceStatus()
@@ -17,19 +18,19 @@ export default function Dashboard() {
       </div>
 
       {/* service cards */}
-      {!isLoading && services.length > 0 && (
-        <section className="grid sm:grid-cols-2 md:grid-cols-3 gap-6 justify-center items-center">
-          {services.map(service => (
+      <section className="grid sm:grid-cols-2 md:grid-cols-3 gap-6 justify-center items-center">
+        {isLoading ? (
+          // Exibe skeletons enquanto carrega
+          Array.from({ length: 6 }).map((_, index) => (
+            <ServiceCardSkeleton key={index} />
+          ))
+        ) : (
+          // Exibe os serviços quando carregados
+          services.map(service => (
             <ServiceCard key={service.id} service={service} />
-          ))}
-        </section>
-      )}
-
-      {isLoading && (
-        <div className="text-center py-8">
-          <p>Carregando serviços...</p>
-        </div>
-      )}
+          ))
+        )}
+      </section>
     </div>
   );
 }
---
+++ src/components/ServiceCard/ServiceCardSkeleton/index.tsx
@@ -0,0 +1,25 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
+export default function ServiceCardSkeleton() {
+  return (
+    <div className="border border-border drop-shadow-sm rounded-lg col-span-1 px-5 py-4 max-w-[300px] md:max-w-full hover:scale-105 transition-all">
+      <div className="flex flex-col gap-2 w-full">
+        <Skeleton className="h-5 w-1/2" />
+        <div className="flex justify-between">
+          <div className="flex flex-col gap-2">
+            <Skeleton className="h-5 w-10" />
+            <Skeleton className="h-5 w-20" />
+          </div>
+
+          <div className="flex flex-col gap-2">
+            <Skeleton className="h-5 w-10" />
+            <Skeleton className="h-5 w-20" />
+          </div>
+        </div>
+
+        {/* last checked */}
+        <Skeleton className="self-end h-5 w-2/3" />
+      </div>
+    </div>
+  )
+}
